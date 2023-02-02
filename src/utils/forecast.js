7be33bb4da3e2084447c9b19a6cd8df1b0d7aa17
Maryia Hancharevich <maryiahancharevich@maryias-macbook-pro.home>
2023-02-02 19:56:15 +0100

    add more weather info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -22,10 +22,10 @@ const forecast = (latitude, longitude, callback) => {
         if (error) {
             callback('Unable to connect to weather service', undefined)
         } else if (body.error) {
-            console.log(body)
             callback(body.error.info, undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + "\nIt is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees.")
+            //console.log(body)
+            callback(undefined, body.current.weather_descriptions[0] + "It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees." + " The wind speed is " + body.current.wind_speed + ". Cloudcover is " + body.current.cloudcover + ".")
         }
     })
 }
